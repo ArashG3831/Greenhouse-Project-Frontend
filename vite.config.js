d50964b3476f2186cc46435c9be2aa542cecfff5
Arash_g <arash.ganjei92@gmail.com>
2025-04-02 16:17:57 +0330

    build bug

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,16 @@ export default defineConfig({
 	plugins: [sveltekit()], 
 	server : {
 		allowedHosts: true
-	}, 
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name].[hash].js',
+				chunkFileNames: 'assets/[name].[hash].js',
+				assetFileNames: 'assets/[name].[hash].[ext]'
+			}
+		}
+	}
 
 
 
